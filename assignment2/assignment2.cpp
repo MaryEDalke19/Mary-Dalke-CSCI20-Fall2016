80c6f773f032963521eee6852bde6a97ca8d5960
Mary Dalke <mdalke001@student.butte.edu>
2016-10-04 20:24:27 +0000

    assignment2 submission

---
+++ assignment2/assignment2.cpp
@@ -0,0 +1,48 @@
+// Mary Dalke
+// 9-28-16
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function converts miles to yards
+double MilesYardsToYards (int origMiles, int origYards) 
+{
+    
+    int totYards = 0; // Resulting yards
+    
+    totYards = (origMiles * 1760) + origYards;
+    
+    return origYards;
+    
+}
+
+// Function converts yards to miles
+double YardsMilesToMiles (int origYards, int origMiles) 
+{
+    int totMiles = 0;
+    
+    totMiles = (origYards * 0.000568182) + origMiles;
+    
+    return origMiles;
+    
+}
+
+int main () 
+{
+string userMiles;
+string userYards;
+    
+    cout << "How many miles did you go?" << endl;
+    cin >> userMiles;
+    cout << endl;
+    
+    cout << "How many yards did you go?" << endl;
+    cin >> userYards;
+    cout << endl;
+    
+    cout << "From miles to yards, your answer is: " << userMiles * totYards << endl;
+    cout << "From yards to miles, your answer is: " << userYards * totMiles << endl;
+    
+    return 0;
+}
