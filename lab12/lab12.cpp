857ad26b0483f12c01643188beb7adcbcde8a349
Mary Dalke <mdalke001@student.butte.edu>
2016-10-09 23:04:31 +0000

    lab12 submission

---
+++ lab12/lab12.cpp
@@ -0,0 +1,51 @@
+//Mary Dalke
+//10-6-16
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () 
+{
+    string newFriend;
+    
+    cout << "Are you my new friend? Do you like horses or turtle?";
+
+    
+    switch (newFriend)
+    {
+        case horses:
+        cout << "Do you like going outside? Yes or no.";
+        break;
+        case no:
+        cout << "What kind of movies do you like? action or romance.";
+        break;
+        case: action:
+        cout << "I think we'll be good friends.";
+        break;
+        case romance:
+        cout << "Sorry, we don't have a lot in common.";
+        case yes:
+        cout << "Do you like to run? Yes or no.";
+        break;
+        case yes:
+        cout << "Sorry, we don't have a lot in common.";
+        break;
+        case no:
+        cout << "I think we'll be good friends.";
+        break;
+        
+        case turtle:
+        cout << "Do you like work? yes or no.";
+        break;
+        case yes:
+        cout << "Sorry, we don't have a lot in common.";
+        break;
+        case no:
+        cout << "I think we'll be good friends.";
+        break;
+        
+        break;
+    }
+    
+}
