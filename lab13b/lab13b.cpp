f52ba523123a0b5dd8aaa6043635175fc0b46fef
Mary Dalke <mdalke001@student.butte.edu>
2016-10-11 19:26:55 +0000

    lab13b submission

---
+++ lab13b/lab13b.cpp
@@ -0,0 +1,41 @@
+//Mary Dalke
+//10-11-16
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () 
+{
+   string printTime;
+   unsigned int i=0;
+   char currChar='_';
+   
+   cout << "Enter print time: " << endl;
+   cin >> printTime;
+   cout << endl;
+   
+   cout << "Print only: ";
+   for (i = 0, i < printTime.size(); ++i)
+   {
+       currChar = printTime.at(i);
+       if (((currChar >= '0') &&  (currChar<='9') || (currChar == '-'))
+       {
+           cout << currChar;
+       }
+       else if ( ((currChar >= '1:00') && (currChar <= '2:59' )) ||
+                 ((currChar >= '1:00 pm') && (currChar <= '2:59 pm')) )
+                 {
+                     cout << "15 minutes";
+                 }
+        else 
+        {
+            cout << '?';
+            cout << endl;
+        }
+   }
+   
+   cout << endl;
+   
+    return 0;
+}
