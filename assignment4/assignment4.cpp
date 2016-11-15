88ccca13968da7bbbc904426f3bc6852e8e0f0d7
Mary Dalke <mdalke001@student.butte.edu>
2016-11-15 22:27:23 +0000

    assignment4 submission

--- assignment4/assignment4.cpp
+++ assignment4/assignment4.cpp
@@ -6,6 +6,7 @@
 #include <ios>
 #include <iomanip>
 #include <fstream>
+#include <string>
 using namespace std;
 
 class StudentAnswers
@@ -13,7 +14,7 @@ class StudentAnswers
 public:
 void SetGrade (int studGrade);
 void SetStudentAnswers (int studAnswers);
-double GetTeacherAnswers () const;
+double GetTeacherAnswer () const;
 
 private:
 int teacherAnswer;
@@ -25,82 +26,97 @@ void StudentAnswers::SetGrade(int studGrade)
     studentScore = studGrade;
 }
 
-void StudentAnswers::SetStudentAnswers (double studAnswer)
+void StudentAnswers::SetStudentAnswers (int studAnswers)
 {
-    teacherAnswers = studAnswers;
+    teacherAnswer = studAnswers;
 }
 
-double StudentAnswers::GetTeacherAnsers () const
+double StudentAnswers::GetTeacherAnswer () const
 {
-    return teacherAnswers / (studentScore / 100.00);
+    return teacherAnswer / (studentScore / 100.00);
 }
 
 
 int main ()
 {
-    string studentName = "Chirstian Smith";
-    istringstream inSS(studentName);
+    ifstream inFS;
+    char fileNum1 = '';
+    char fileNum2 = '';
+    string filename = "";
+    StudentAnswers student1;
     string firstName;
     string lastName;
     int userGrade = 0;
     
-    inFS.open("studentanswers.txt")
+    cout << "Enter filename: " << endl;
+    cin >> filename;
+    
+    student1.SetGrade(100);
+    student1.SetStudentAnswers(23);
+    cout << "student1's grade: " << student1.GetTeacherAnswer();
+    
+    inFS.open("studentanswers.txt");
     if (!inFS.is_open())
     {
         cout << "Could not open file 'studentanswers.txt'. " << endl;
     }
     
-    inFS.open("answerkey.txt")
+    inFS.open("answerkey.txt");
     if (!inFS.is_open())
     {
         cout << "Could not open file 'answerkey.txt'. " << endl;
     }
     
-    studentanswers.txt = filenum1;
-    answerkey.txt = filenum2;
+    cout << "Read the two files: " << endl;
+    inFS >> fileNum1;
+    inFS >> fileNum2;
+    "fileNum1" == "fileNum2";
+    cout << "Close both files. " << endl;
+    inFS.close(fileNum1);
+    inFS.close(FileNum2);
     
-    inFS >> filenum1;
-    inFS >> filenum2;
+    cout << "FileNum1 output: " << fileNum1 << endl;
+    cout << "FileNum2 output: " << fileNum2 << endl;
     
-    int StudentGrade[11];
-    studentGrade[0] = 100;
-    studentGrade[1] = 90;
-    studentGrade[2] = 80;
-    studentGrade[3] = 70;
-    studentGrade[4] = 60;
-    studentGrade[5] = 50;
-    studentGrade[6] = 40;
-    studentGrade[7] = 30;
-    studentGrade[8] = 20;
-    studentGrade[9] = 10;
-    studentGrade[10] = 0;
+    int grades[11];
+    grades[0] = 100;
+    grades[1] = 90;
+    grades[2] = 80;
+    grades[3] = 70;
+    grades[4] = 60;
+    grades[5] = 50;
+    grades[6] = 40;
+    grades[7] = 30;
+    grades[8] = 20;
+    grades[9] = 10;
+    grades[10] = 0;
     
-    if (studentGrade <= 100)
+    if (grades[0] <= 100)
     {
         cout << "Great job! You got an A!" << endl;
     }
     
-    if else (studentGrade <= 90)
+    else if (grades[1] <= 90)
     {
         cout << "Great job! You got an A!" << endl;
     }
     
-    if else (studentGrade <= 80)
+    else if (grades[2] <= 80)
     {
         cout << "Good job! You got an B!" << endl;
     }
     
-    if else (studentGrade <= 70)
+    else if (grades[3] <= 70)
     {
         cout << "Good job! You got an C!" << endl;
     }
     
-    if else (studentGrade <= 60)
+    else if (grades[4] <= 60)
     {
         cout << "Good job! You got an D!" << endl;
     }
     
-    if else (studentGrade <= 50)
+    else if (grades[5] <= 50)
     {
         cout << "Bad job! You got an F!" << endl;
     }
